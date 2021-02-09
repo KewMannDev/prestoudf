892673a2d6ae1b461bbcba6a474d934b62d537f4
CollinKendor <ellessar.kl@gmail.com>
2021-02-09 11:37:01 +0800

    Modified stressTestRealTime() to create and close JDBC connection in each loop.

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -138,25 +138,25 @@ public class UnitTest {
      */
     @Test
     public void stressTestRealTime() throws SQLException {
-        Connection connection = jdbcConnection();
-        Statement stmt = getStatement(connection);
-        String sql = "INSERT INTO bulk_100 VALUES(|row|,'os5wRVI8BhjtLUNhisuOA4trKjTPC6/FX+vvIrqlMeU=','Cx9kazlF9iBX8+GstGfzmg=','On46L6igJQl7PZ/snBPoZA==','On46L6igJQl7PZ/snBPoZA==','On46L6igJQl7PZ/snBPoZA==','GhgsnLAyrvt/cFubrTTJJg==','YePvTywrn1cI/uaz67Y7tA==','0rx4QxnflHc5ih1ledOZ1w==','T0b56F3RG+bNxvjIZ6xBOQ==','Fc+x3DhB8uKSRRxEzOTFXQ==','aEShsTcK6h8SpcOYC4xjJQ==','M/cxFtUM1jaL/WWtGNLKnvcuPLHcESKcvnh7TYp4Awg=','wwn+Xc80NKR8Rhnz/mtC3M8qXlr4wRTq/36kOgp0WBI=','T0b56F3RG+bNxvjIZ6xBOQ==')";
-
-        connection.setAutoCommit(false);
         int i = 0;
-        try {
-            while(true) {
+        while(true) {
+            Connection connection = jdbcConnection();
+            Statement stmt = getStatement(connection);
+            String sql = "INSERT INTO bulk_100 VALUES(|row|,'os5wRVI8BhjtLUNhisuOA4trKjTPC6/FX+vvIrqlMeU=','Cx9kazlF9iBX8+GstGfzmg=','On46L6igJQl7PZ/snBPoZA==','On46L6igJQl7PZ/snBPoZA==','On46L6igJQl7PZ/snBPoZA==','GhgsnLAyrvt/cFubrTTJJg==','YePvTywrn1cI/uaz67Y7tA==','0rx4QxnflHc5ih1ledOZ1w==','T0b56F3RG+bNxvjIZ6xBOQ==','Fc+x3DhB8uKSRRxEzOTFXQ==','aEShsTcK6h8SpcOYC4xjJQ==','M/cxFtUM1jaL/WWtGNLKnvcuPLHcESKcvnh7TYp4Awg=','wwn+Xc80NKR8Rhnz/mtC3M8qXlr4wRTq/36kOgp0WBI=','T0b56F3RG+bNxvjIZ6xBOQ==')";
+
+            connection.setAutoCommit(false);
+            try {
                 stmt.execute(sql.replace("|row|", String.valueOf(i)));
                 connection.commit();
                 i++;
             }
-        }
-        catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        finally {
-            connection.close();
-            stmt.close();
+            catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            finally {
+                connection.close();
+                stmt.close();
+            }
         }
     }
 
